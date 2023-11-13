c0ca07196c116d573db9bfe0c61909a8c9118f54
Talib Smith <talibsmith77@gmail.com>
2023-11-13 14:03:15 -0500

    page setups

--- enjoy-the-outdoors/scripts/parkTypeData.js
+++ enjoy-the-outdoors/scripts/parkTypeData.js
@@ -1,15 +1,30 @@
 const parkTypesArray = [
-    "National Park",
-    "National Monument",
-    "Recreation Area",
-    "Scenic Trail",
-    "Battlefield",
-    "Historic",
-    "Memorial",
-    "Preserve",
-    "Island",
-    "River",
-    "Seashore",
-    "Trail",
-    "Parkway"
-]
+  "National Park",
+  "National Monument",
+  "Recreation Area",
+  "Scenic Trail",
+  "Battlefield",
+  "Historic",
+  "Memorial",
+  "Preserve",
+  "Island",
+  "River",
+  "Seashore",
+  "Trail",
+  "Parkway",
+];
+
+
+
+function loadParkTypes() {
+const parkTypeList = document.getElementById("parkTypeList")
+
+  for (const park of parkTypesArray) {
+    let option = document.createElement("option")
+    option.value= park
+    option.innerText = park
+    
+    parkTypeList.appendChild(option);
+  }
+}
+loadParkTypes();
